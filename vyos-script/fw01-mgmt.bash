5c307623c8ff8cf2c7a65a858f3636f58e44344b
Paiet <78005127+Paiet@users.noreply.github.com>
2021-10-26 02:28:32 -0400

    Update fw01-mgmt.bash

--- vyos-script/fw01-mgmt.bash
+++ vyos-script/fw01-mgmt.bash
@@ -4,25 +4,24 @@ configure
 
 # MGMT-to-LAN
 # Rule 1
-Allow all connections
+allow all connections
 set firewall name MGMT-to-LAN rule 1 action accept
 
 # LAN-to-MGMT
 # Rule 1
-Allow established connections
+allow established connections
 set firewall name LAN-to-MGMT rule 1 action accept
 set firewall name LAN-to-MGMT rule 1 state established enable
 # Rule 10
-Allow established connections
+allow established connections
 set firewall name LAN-to-MGMT rule 10 action accept
 set firewall name LAN-to-MGMT rule 10 state established enable
 # Rule 1
-Allow established connections
+allow established connections
 set firewall name LAN-to-MGMT rule 20 action accept
 set firewall name LAN-to-MGMT rule 20 destination address 10.0.5.10
 set firewall name LAN-to-MGMT rule 20 destination port 8089
 set firewall name LAN-to-MGMT rule 20 protocol tcp
 
 commit
-save
 exit
